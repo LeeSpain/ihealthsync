6cf4ae6f9460be7527dbcdf0b86dcfd118a5d347
Your Name <your-email@example.com>
2025-01-12 13:23:18 +0100

    Updates

--- backend/api/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { loginUser } = require('../controllers/userController'); // Corrected path
-
-const router = express.Router();
-
-// Define the login route
-router.post('/login', loginUser);
-
-module.exports = router;
